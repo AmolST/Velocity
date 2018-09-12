526bf8e2f9922f7471071d154968de67d2c9166e
D:\Namrata Work <amol.thikane@gmail.com>
2018-09-12 20:06:13 +1000

    Implmented heart rating

--- star-rating.directive.js
+++ star-rating.directive.js
@@ -2,10 +2,19 @@ app.directive('starRating', [function () {
     return {
         restrict: 'EA',
         templateUrl : './star-rating.template.html',
-        scope: {           
+        scope: {        
+            ratingValue : '=ratingvalue'   
           },
         link :function(scope, element, attributes) {
-     
+        if (scope.max == undefined) {
+            scope.max = 5;
+            }
+            scope.stars = [];
+            for (var i = 0; i < scope.max; i++) {
+                scope.stars.push({
+                filled: i < scope.ratingValue
+                });
+            }
         }
     }
 }]);
