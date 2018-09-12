8f5c505448a22d8beb0edbca2b786697ed54bb26
Amol Thikane <amol.thikane@gmail.com>
2018-09-12 18:16:13 +1000

    Added star directive

---
+++ star-rating.directive.js
@@ -0,0 +1,11 @@
+app.directive('starRating', [function () {
+    return {
+        restrict: 'EA',
+        templateUrl : './star-rating.template.html',
+        scope: {           
+          },
+        link :function(scope, element, attributes) {
+     
+        }
+    }
+}]);
