e4cf8ec73b3ad1fe545f5c89812fc0ca4c450763
D:\Namrata Work <amol.thikane@gmail.com>
2018-09-12 20:17:12 +1000

    Fixed rating update on contact click

--- star-rating.directive.js
+++ star-rating.directive.js
@@ -1,20 +1,27 @@
 app.directive('starRating', [function () {
     return {
         restrict: 'EA',
-        templateUrl : './star-rating.template.html',
-        scope: {        
-            ratingValue : '=ratingvalue'   
-          },
-        link :function(scope, element, attributes) {
-        if (scope.max == undefined) {
-            scope.max = 5;
+        templateUrl: './star-rating.template.html',
+        scope: {
+            ratingValue: '=ratingvalue'
+        },
+        link: function (scope, element, attributes) {
+            if (scope.max == undefined) {
+                scope.max = 5;
             }
-            scope.stars = [];
-            for (var i = 0; i < scope.max; i++) {
-                scope.stars.push({
-                filled: i < scope.ratingValue
-                });
-            }
+
+            scope.$watch('ratingValue', function () {
+                scope.updateStars();
+            });
+
+           scope.updateStars = function () {
+                scope.stars = [];
+                for (var i = 0; i < scope.max; i++) {
+                    scope.stars.push({
+                        filled: i < scope.ratingValue
+                    });
+                }
+            };
         }
     }
 }]);
